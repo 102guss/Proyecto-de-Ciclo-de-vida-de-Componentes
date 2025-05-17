0c9a62253321fde5e8afb4d27954a2df3738efca
102guss <brown_321@live.com>
2025-05-16 23:39:17 -0600

    Código empezado a partir de propuesta

--- explorador-espacial/src/App.jsx
+++ explorador-espacial/src/App.jsx
@@ -1,11 +1,37 @@
-import './App.css'
+// App.jsx
+import React, { useState, useEffect, useMemo } from 'react'
+import Planeta from './Planeta'
 
 function App () {
-  return (
-    <>
+  // ... (estado)
 
-    </>
+  useEffect(() => {
+    console.log('¡El panel está listo!') // Montaje
+
+    const intervalo = setInterval(() => { // Montaje
+      // ... (simulación de vuelo)
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalo) // Desmontaje
+      console.log('El panel se ha apagado.') // Desmontaje
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log('¡Combustible actualizado!') // Actualización
+  }, [combustible])
+
+  const mensajeEstado = useMemo(() => {
+    return `Estado: ${estadoNave}`
+  }, [estadoNave])
+
+  return (
+    <div>
+      {/* ... (información del panel) */}
+      {planetasVisitados.map((planeta, index) => (
+        <Planeta key={index} nombre={planeta} />
+      ))}
+    </div>
   )
 }
-
-export default App
